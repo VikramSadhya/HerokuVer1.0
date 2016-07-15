5e0835454478689e9d1284e7a0957aec05166057
VikramSadhya <vsadhy1@lsu.edu>
2016-07-15 12:30:41 -0500

    Uploading files

---
+++ server.js
@@ -0,0 +1,59 @@
+var express = require('express');
+var connect = require('connect');
+var path = require('path');
+var app = express();
+var bodyParser = require('body-parser');
+var mysql = require('mysql');
+
+var port = Number(process.env.PORT || 5000);
+
+var pool = mysql.createPool({
+host:'db.cct.lsu.edu',
+user:'vsadhya',
+password:'abcdef67',
+database:'beacon',
+port: 3306,
+debug: false,
+connectionLimit: 100
+});
+
+function handle_database(req,res) {
+    pool.getConnection(function(err,connection){
+        if (err) {
+          connection.release();
+          console.log('Database Connection failed');
+          return;
+        } else {
+          console.log('Connected to database'); 
+        }
+        });
+  };
+
+
+app.use(function(req, res, next) {
+  console.log(`${req.method} request for ${req.url}`);
+  next();
+
+});
+
+//app.use(bodyParser.urlencoded({extended: false}));
+//app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+  handle_database();
+  pool.query('SELECT * FROM checkin', function(err, result) {
+    if(err){
+        throw err;
+      } else {
+          res.send(result);                
+        }
+    });
+});
+
+app.listen(port, function(err, req, res){
+  if (err){
+    console.log("Failed to start app at port:%s", port);
+  } else {
+    console.log("Project app running at port:%s", port);
+  }
+});
